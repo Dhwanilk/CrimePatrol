ae11082946341e315546c80fd4bad0cfdcd5ce7a
Dhwanilk <dhwanilk@gmail.com>
2016-01-27 16:42:04 -0800

    Added code to perform simple network request

--- CrimePatrol/CPCrimeListDataManager.m
+++ CrimePatrol/CPCrimeListDataManager.m
@@ -13,8 +13,8 @@ static NSString* const kBaseURLString = @"https://data.sfgov.org/resource/cuks-n
 static NSString* const kAppTokenKey = @"$$app_token";
 static NSString* const kAppTokenValue = @"b4KsWInZBzXo1X7m69nPOBDX3";
 
-static NSString* const kWhere = @"where";
-static NSString* const kDateRange = @"date between '2015-12-27T12:00:00' and '2016-01-27T14:00:00'";
+static NSString* const kWhere = @"$where";
+static NSString* const kDateRange = @"date between \'2015-12-27T12:00:00\' and \'2016-01-27T14:00:00\'";    //Add configuration options
 
 static NSString* const kOffset = @"$offset";
 static NSString* const kLimit = @"$limit";
@@ -43,27 +43,32 @@ static NSString* const kLimit = @"$limit";
     return  self;
 }
 
-- (NSString *)getBaseURLWithAppToken {
-    return [NSString stringWithFormat:@"%@%@=%@", kBaseURLString, kAppTokenKey, kAppTokenValue];
-}
-
 - (void)loadData {
     //Sample URL
-    //https://data.sfgov.org/resource/cuks-n6tp.json?$$app_token=b4KsWInZBzXo1X7m69nPOBDX3&$where=date between '2015-12-27T12:00:00' and '2016-01-27T14:00:00'&$offset=0&$limit=20"
     
-    
+    //https://data.sfgov.org/resource/cuks-n6tp.json?$$app_token=b4KsWInZBzXo1X7m69nPOBDX3&$where=date between '2015-12-27T12:00:00' and '2016-01-27T14:00:00'&$offset=0&$limit=20
     
     NSString *baseURL = [self getBaseURLWithAppToken];
-    [baseURL stringByAppendingFormat:@"&%@=%@&%@=%d&%@=%d",kWhere, kDateRange, kOffset, 0, kLimit, 20];
+    NSString *dateOffsetURLString = [baseURL stringByAppendingFormat:@"&%@=%@&%@=%d&%@=%d",kWhere, kDateRange, kOffset, 0, kLimit, 20];
     
-    NSURLSessionDataTask *dataTask = [self.sharedSession dataTaskWithURL:[NSURL URLWithString:baseURL]
+    //Escape string so that the date range works with quotes
+    NSString *escapedTitle = [dateOffsetURLString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    
+    NSURLSessionDataTask *dataTask = [self.sharedSession dataTaskWithURL:[NSURL URLWithString:escapedTitle]
                                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        NSLog(@"JSON-----\n%@", json);
+                                                           
+                                                           if (!error) {
+                                                               NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                                               NSLog(@"JSON-----\n%ld\n%@", [jsonArray count], jsonArray);
+                                                               
+                                                           } else {
+                                                               NSLog(@"Error: %@", [error localizedDescription]);
+                                                           }
     }];
     
     [dataTask resume];
 }
+
 
 - (NSInteger)numberOfItems {
     return 0;
@@ -81,6 +86,13 @@ static NSString* const kLimit = @"$limit";
 - (BOOL)shouldFetchMoreForIndex:(NSInteger)index {
     return false;
 }
+
+#pragma mark - Private Methods
+
+- (NSString *)getBaseURLWithAppToken {
+    return [NSString stringWithFormat:@"%@%@=%@", kBaseURLString, kAppTokenKey, kAppTokenValue];
+}
+
 
 
 @end
